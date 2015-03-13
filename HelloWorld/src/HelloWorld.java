4bc6bf191e562c9e051402f499f425ec2c525667
Hanmyokyung <ccunyday@naver.com>
2015-03-13 15:49:51 +0900

    testing_3

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String args[]){
 		System.out.println("testing_1");
 		System.out.println("testing_2");
+		System.out.println("testing_3");
 	}
 
 }
